1cab35d142200c733b0cd21dda2bde6dc10c8505
Caleb F <1calebasc@gmail.com>
2026-08-03 14:01:48 -0500

    data.js

--- data.js
+++ data.js
@@ -1,1 +1,290 @@
-
+{
+  "results": [
+    {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Rodrigo",
+        "last": "Valverde"
+      },
+      "location": {
+        "street": {
+          "number": 9116,
+          "name": "Circuito San Luis Potosí"
+        },
+        "city": "Rancho Contento",
+        "state": "Hidalgo",
+        "country": "Mexico",
+        "postcode": 76461,
+        "coordinates": {
+          "latitude": "29.0147",
+          "longitude": "-24.9336"
+        },
+        "timezone": {
+          "offset": "-9:00",
+          "description": "Alaska"
+        }
+      },
+      "email": "rodrigo.valverde@example.com",
+      "login": {
+        "uuid": "de1e6a6b-7a4a-49aa-bd18-15b119bc778d",
+        "username": "biglion166",
+        "password": "sluggo",
+        "salt": "BOsQLQN0",
+        "md5": "32d02bc0b4281b629fff1d162d85d9fe",
+        "sha1": "da85069f0c8a08d1e3e17ffe0759d92131eae7cb",
+        "sha256": "236738599fd574ba46a9615e972b36c4ac5b0cb678ffb22509a1711e5eb0550f"
+      },
+      "dob": {
+        "date": "1964-08-21T03:05:47.905Z",
+        "age": 61
+      },
+      "registered": {
+        "date": "2016-05-13T12:08:00.470Z",
+        "age": 10
+      },
+      "phone": "(605) 774 4586",
+      "cell": "(633) 669 9267",
+      "id": {
+        "name": "NSS",
+        "value": "92 45 75 4171 7"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/11.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/11.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/11.jpg"
+      },
+      "nat": "MX"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Jessie",
+        "last": "Gonzales"
+      },
+      "location": {
+        "street": {
+          "number": 6225,
+          "name": "Cherry St"
+        },
+        "city": "Gladstone",
+        "state": "New South Wales",
+        "country": "Australia",
+        "postcode": 7526,
+        "coordinates": {
+          "latitude": "18.4412",
+          "longitude": "-39.8555"
+        },
+        "timezone": {
+          "offset": "+11:00",
+          "description": "Magadan, Solomon Islands, New Caledonia"
+        }
+      },
+      "email": "jessie.gonzales@example.com",
+      "login": {
+        "uuid": "5aaef4ac-f379-4c86-9c2b-0185f9163d93",
+        "username": "crazyostrich279",
+        "password": "trance",
+        "salt": "LmFapClV",
+        "md5": "a18359c518c0420403e25a9948006d2d",
+        "sha1": "1e2b0ab6972f3bcd4726020b5721ef993445b46b",
+        "sha256": "429b8c5eb4903b2e145321b7501266f11aca33b1ccfe9b1ef05d0c1b544656ff"
+      },
+      "dob": {
+        "date": "1986-09-08T04:02:17.767Z",
+        "age": 39
+      },
+      "registered": {
+        "date": "2012-09-07T22:04:59.259Z",
+        "age": 13
+      },
+      "phone": "04-4254-6964",
+      "cell": "0482-366-700",
+      "id": {
+        "name": "TFN",
+        "value": "408290114"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/4.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/4.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/4.jpg"
+      },
+      "nat": "AU"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "Ms",
+        "first": "Nellie",
+        "last": "Stewart"
+      },
+      "location": {
+        "street": {
+          "number": 3631,
+          "name": "Fairview St"
+        },
+        "city": "Melbourne",
+        "state": "Tasmania",
+        "country": "Australia",
+        "postcode": 5688,
+        "coordinates": {
+          "latitude": "0.4839",
+          "longitude": "-176.1957"
+        },
+        "timezone": {
+          "offset": "0:00",
+          "description": "Western Europe Time, London, Lisbon, Casablanca"
+        }
+      },
+      "email": "nellie.stewart@example.com",
+      "login": {
+        "uuid": "372ce4a3-b806-4639-baea-7e46aac1160d",
+        "username": "goldengoose795",
+        "password": "fender1",
+        "salt": "WIKW11y4",
+        "md5": "a2c8bab9b3ef33261c75b99089ebad28",
+        "sha1": "379a9e7a5b5b179effa6b80854d81fbed890dfee",
+        "sha256": "d1cc9dc45f269ec810e508a25874d5550629e04d77b89183088a5c1485562f13"
+      },
+      "dob": {
+        "date": "1965-07-10T23:33:45.057Z",
+        "age": 61
+      },
+      "registered": {
+        "date": "2021-07-11T21:03:20.666Z",
+        "age": 5
+      },
+      "phone": "01-8717-4486",
+      "cell": "0424-179-454",
+      "id": {
+        "name": "TFN",
+        "value": "730261985"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/women/50.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/50.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/50.jpg"
+      },
+      "nat": "AU"
+    },
+    {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Branko",
+        "last": "Jakšić"
+      },
+      "location": {
+        "street": {
+          "number": 1519,
+          "name": "Vrbeštička"
+        },
+        "city": "Vršac",
+        "state": "Pirot",
+        "country": "Serbia",
+        "postcode": 28444,
+        "coordinates": {
+          "latitude": "-46.5187",
+          "longitude": "150.1082"
+        },
+        "timezone": {
+          "offset": "+5:45",
+          "description": "Kathmandu"
+        }
+      },
+      "email": "branko.jaksic@example.com",
+      "login": {
+        "uuid": "1236b08b-db75-449a-8c89-cdd6e555c5f8",
+        "username": "bluetiger424",
+        "password": "flash1",
+        "salt": "Fo0isHmP",
+        "md5": "9429ab004e01057099c5275c000fa88b",
+        "sha1": "86fffe5b43c2ea00ab6bdc3ecd5affc62a430e5d",
+        "sha256": "085f62de2f898366cf1f6482aa5d807ee69e8f4e37a90f958444d29aeb3544c7"
+      },
+      "dob": {
+        "date": "1957-06-18T10:54:30.239Z",
+        "age": 69
+      },
+      "registered": {
+        "date": "2015-05-13T13:24:59.876Z",
+        "age": 11
+      },
+      "phone": "024-1943-242",
+      "cell": "068-8162-562",
+      "id": {
+        "name": "SID",
+        "value": "043361187"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/83.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/83.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/83.jpg"
+      },
+      "nat": "RS"
+    },
+    {
+      "gender": "female",
+      "name": {
+        "title": "Mrs",
+        "first": "Abigail",
+        "last": "Smith"
+      },
+      "location": {
+        "street": {
+          "number": 8333,
+          "name": "Regent Ave"
+        },
+        "city": "Trenton",
+        "state": "Saskatchewan",
+        "country": "Canada",
+        "postcode": "P5F 1N5",
+        "coordinates": {
+          "latitude": "-67.7042",
+          "longitude": "108.7258"
+        },
+        "timezone": {
+          "offset": "-5:00",
+          "description": "Eastern Time (US & Canada), Bogota, Lima"
+        }
+      },
+      "email": "abigail.smith@example.com",
+      "login": {
+        "uuid": "b2c77da3-72be-4696-8503-a2fd98d113b2",
+        "username": "brownzebra208",
+        "password": "stimpy",
+        "salt": "oT17TfUf",
+        "md5": "c33e019ad9638fab5b298eac7ed6dbb0",
+        "sha1": "81eac959382401985f369389e36b12a9b1dabf3f",
+        "sha256": "3432afd0b1e0c5d3348c942f1c058ad273cf84d02637d7e1ba430ced8989403c"
+      },
+      "dob": {
+        "date": "1965-02-08T13:26:50.213Z",
+        "age": 61
+      },
+      "registered": {
+        "date": "2016-03-06T21:07:29.809Z",
+        "age": 10
+      },
+      "phone": "K54 O38-1133",
+      "cell": "V01 J69-8233",
+      "id": {
+        "name": "SIN",
+        "value": "804618387"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/women/25.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/women/25.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/women/25.jpg"
+      },
+      "nat": "CA"
+    }
+  ],
+  "info": {
+    "seed": "939a9f2e8266ee7d",
+    "results": 5,
+    "page": 1,
+    "version": "1.4"
+  }
+}
