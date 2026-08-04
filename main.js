dc5d42d1c0deea82082190aa9337313e8ecfcd09
1calebasc-max <1calebasc@gmail.com>
2026-08-04 13:47:52 -0500

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
+const usersContainer = document.getElementById("users");
 
+
+function displayUsers(){
+
+    usersContainer.innerHTML = "";
+
+
+    data.results.forEach(function(user){
+
+
+        let card = document.createElement("div");
+
+        card.classList.add("card");
+
+
+        card.innerHTML = `
+
+            <img src="${user.picture.large}">
+
+            <h2>
+            ${user.name.first} ${user.name.last}
+            </h2>
+
+            <p>
+            Age: ${user.dob.age}
+            </p>
+
+            <p>
+            ${user.location.city}, 
+            ${user.location.country}
+            </p>
+
+            <p>
+            ${user.email}
+            </p>
+
+        `;
+
+
+        usersContainer.appendChild(card);
+
+    });
+
+}
+
+
+displayUsers();
